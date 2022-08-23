ffc233164d798c1ef32e3b85c7272a1bff8a5d51
arominddo <godhsb@naver.com>
2022-08-23 16:41:07 +0900

    2022.08.23.(3)

---
+++ Java_jungsuk_practice/src/OverflowEX.java
@@ -0,0 +1,17 @@
+
+public class OverflowEX {
+
+	public static void main(String[] args) {
+		short sMin = -32768;
+		short sMax = 32767;
+		char cMin = 0;
+		char cMax = 65535;
+		
+		System.out.println("sMin = " + sMin);
+		System.out.println("smin-1 = " + (short)(sMin-1));
+		System.out.println("sMax = " + sMax);
+		System.out.println("sMax+1 = " + (short)(sMax+1));
+
+	}
+
+}
---
+++ Java_jungsuk_practice/src/VarEx2.java
@@ -0,0 +1,14 @@
+
+public class VarEx2 {
+
+	public static void main(String[] args) {
+		double x = 1.0f;
+		double y = x / 3.0f;
+		System.out.println("x:"+x);
+		System.out.println("y:"+y);
+		System.out.println("y*3.0:"+y*3.0);
+		
+		System.out.println();
+	}
+
+}
