7205fa968ca6a688e0b6328461bf110a8de4cc10
arominddo <godhsb@naver.com>
2022-08-29 18:03:48 +0900

    2022.08.29.(4)

--- Java_jungsuk_practice/src_EX/ch06/CardTest.java
+++ Java_jungsuk_practice/src_EX/ch06/CardTest.java
@@ -1,3 +1,5 @@
+package ch06;
+
 class CardTest{
 	public static void main(String args[]) {
 		System.out.println("Card.width = "  + Card.width);
@@ -12,7 +14,7 @@ class CardTest{
 		c2.number = 4;
 
 		System.out.println("c1은 " + c1.kind + ", " + c1.number + "이며, 크기는 (" + c1.width + ", " + c1.height + ")" );
-		System.out.println("c2는 " + c2.kind + ", " + c2.number + "이며, 크기는 (" + c2.width + ", " + c2.height + ")" );		
+		System.out.println("c2는 " + c2.kind + ", " + c2.number + "이며, 크기는 (" + Card.width + ", " + c2.height + ")" );		
 
 		System.out.println("c1의 width와 height를 각각 50, 80으로 변경합니다.");
 		c1.width = 50;
