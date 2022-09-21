5a8eade780be8c03287e660e61135f265e4fe5ac
arominddo <godhsb@naver.com>
2022-09-21 18:39:02 +0900

    2022.09.21.(1)

---
+++ Java_jungsuk_practice/src_Practice/ch13/Exercise13_1.java
@@ -0,0 +1,22 @@
+package ch13;
+
+public class Exercise13_1 {
+
+	public static void main(String[] args) {
+		Thread1 th1 = new Thread1();
+		
+		th1.start();
+
+	}
+
+}
+
+
+
+class Thread1 implements Runnable{
+	public void  run() {
+		for(int i=0; i<300; i++) {
+			System.out.print('-');
+		}
+	}
+}
